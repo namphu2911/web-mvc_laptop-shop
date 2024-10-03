ec5fb67158dbe9e189af99a7222fb3da89719521
namphu2911 <kuboy151@gmail.com>
2024-10-03 23:24:44 +0700

    done theory

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,6 +8,16 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World Nam Phu!";
+        return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello World User!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Hello World Admin!";
     }
 }
