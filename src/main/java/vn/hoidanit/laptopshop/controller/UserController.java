eff84e5449abe86b7d21de40f7cb3ebb9188caa1
namphu2911 <kuboy151@gmail.com>
2024-10-06 00:22:06 +0700

    jsp and jstl

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,40 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping
+    public String getHomePage(Model model) {
+        String message = this.userService.handleHello();
+        model.addAttribute("message", message);
+        return "hello";
+    }
+}
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%> <%@ taglib prefix="c"
+uri="http://java.sun.com/jsp/jstl/core"%>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Eric's Page</title>
+  </head>
+  <body>
+    Hello from JSP!
+    <h1>${message}</h1>
+  </body>
+</html>
