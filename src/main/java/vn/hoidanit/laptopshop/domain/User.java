40ca8cbb23d3ccfe676e1bef05e6a10032d055f9
namphu2911 <kuboy151@gmail.com>
2024-10-21 00:27:12 +0700

    one - many relationship setup

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,33 +1,40 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email;
+
     private String password;
     private String fullName;
     private String address;
     private String phone;
 
-    // public User() {
-    // }
+    private String avatar;
 
-    // public User(long id, String email, String password, String fullName, String
-    // address, String phone) {
-    // this.id = id;
-    // this.email = email;
-    // this.password = password;
-    // this.fullName = fullName;
-    // this.address = address;
-    // this.phone = phone;
-    // }
+    // roleId
+    // User many -> to one Role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
     public long getId() {
         return id;
@@ -77,9 +84,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
-        return "User [id=" + id + ", email=" + email + ", password="
-                + password + ", fullName=" + fullName + ", address=" + address + ", phone=" + phone + "]";
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+
 }
