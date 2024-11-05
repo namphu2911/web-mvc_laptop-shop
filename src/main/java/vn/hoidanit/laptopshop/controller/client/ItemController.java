c7a8e15dafc02ee76775c7209cce4a8bd6d09ae3
namphu2911 <kuboy151@gmail.com>
2024-11-06 00:50:41 +0700

    add laptop product fake data, done product admin, show laptop on user view

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,15 +1,28 @@
 package vn.hoidanit.laptopshop.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
 
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
+        Product product = this.productService.fetchProductById(id).get();
+        model.addAttribute("product", product);
         return "client/product/detail";
     }
 }
