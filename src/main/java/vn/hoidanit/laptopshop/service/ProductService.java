6f5f23ffae401123b93b24f98be526b3e7a32776
namphu2911 <kuboy151@gmail.com>
2024-11-17 23:22:32 +0700

    add to cart (data handle only)

--- src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -5,15 +5,27 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.Cart;
+import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.CartDetailRepository;
+import vn.hoidanit.laptopshop.repository.CartRepository;
 import vn.hoidanit.laptopshop.repository.ProductRepository;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CartRepository cartRepository;
+    private final CartDetailRepository cartDetailRepository;
+    private final UserService userService;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CartRepository cartRepository,
+            CartDetailRepository cartDetailRepository, UserService userService) {
         this.productRepository = productRepository;
+        this.cartRepository = cartRepository;
+        this.cartDetailRepository = cartDetailRepository;
+        this.userService = userService;
     }
 
     public Product createProduct(Product pr) {
@@ -30,5 +42,33 @@ public class ProductService {
 
     public void deleteProduct(long id) {
         this.productRepository.deleteById(id);
+    }
+
+    public void handleAddProductToCart(String email, long productId) {
+        User user = this.userService.getUserByEmail(email);
+        if (user != null) {
+            Cart cart = this.cartRepository.findByUser(user);
+            if (cart == null) {
+                cart = new Cart();
+                cart.setUser(user);
+                cart.setSum(1);
+                this.cartRepository.save(cart);
+            }
+
+            var product = this.productRepository.findById(productId);
+            if (product.isPresent()) {
+                CartDetail cartDetail = new CartDetail();
+                cartDetail.setCart(cart);
+                cartDetail.setProduct(product.get());
+                cartDetail.setPrice(product.get().getPrice());
+                cartDetail.setQuantity(1);
+                this.cartDetailRepository.save(cartDetail);
+            }
+        }
+
+        // CartDetail cartDetail = new CartDetail();
+        // cartDetail.setCart(cart);
+        // cartDetail.setProduct(new Product(productId));
+        // this.cartDetailRepository.save(cartDetail);
     }
 }
