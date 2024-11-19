733baf7b34e87c3478c036cfaf8c0a1f384c93e0
namphu2911 <kuboy151@gmail.com>
2024-11-19 07:46:20 +0700

    add product into cart, delete from cart

--- src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.servlet.http.HttpSession;
 import vn.hoidanit.laptopshop.domain.Cart;
 import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
@@ -44,31 +45,70 @@ public class ProductService {
         this.productRepository.deleteById(id);
     }
 
-    public void handleAddProductToCart(String email, long productId) {
+    public void handleAddProductToCart(String email, long productId, HttpSession session) {
+        // get logged user by email
         User user = this.userService.getUserByEmail(email);
         if (user != null) {
+            // check if user has cart
             Cart cart = this.cartRepository.findByUser(user);
             if (cart == null) {
+                // create new cart
                 cart = new Cart();
                 cart.setUser(user);
-                cart.setSum(1);
+                cart.setSum(0);
                 this.cartRepository.save(cart);
             }
 
-            var product = this.productRepository.findById(productId);
-            if (product.isPresent()) {
-                CartDetail cartDetail = new CartDetail();
-                cartDetail.setCart(cart);
-                cartDetail.setProduct(product.get());
-                cartDetail.setPrice(product.get().getPrice());
-                cartDetail.setQuantity(1);
-                this.cartDetailRepository.save(cartDetail);
+            // already has cart, add product to cart
+            var optionalProduct = this.productRepository.findById(productId);
+            if (optionalProduct.isPresent()) {
+                Product product = optionalProduct.get();
+
+                // check if product already in cart
+                CartDetail cartDetail = this.cartDetailRepository.findByCartAndProduct(cart, product);
+                if (cartDetail == null) {
+                    // add new product to cart
+                    cartDetail = new CartDetail();
+                    cartDetail.setCart(cart);
+                    cartDetail.setProduct(product);
+                    cartDetail.setPrice(product.getPrice());
+                    cartDetail.setQuantity(1);
+                    this.cartDetailRepository.save(cartDetail);
+
+                    // update cart sum
+                    int sum = cart.getSum() + 1;
+                    cart.setSum(sum);
+                    this.cartRepository.save(cart);
+                    session.setAttribute("sum", sum);
+                } else {
+                    // increase quantity of product in cart
+                    cartDetail.setQuantity(cartDetail.getQuantity() + 1);
+                    this.cartDetailRepository.save(cartDetail);
+                }
             }
         }
+    }
 
-        // CartDetail cartDetail = new CartDetail();
-        // cartDetail.setCart(cart);
-        // cartDetail.setProduct(new Product(productId));
-        // this.cartDetailRepository.save(cartDetail);
+    public Cart fetchByUser(User user) {
+        return this.cartRepository.findByUser(user);
+    }
+
+    public void handleRemoveCartDetail(long cartDetailId, HttpSession session) {
+        Optional<CartDetail> optionalCartDetail = this.cartDetailRepository.findById(cartDetailId);
+        if (optionalCartDetail.isPresent()) {
+            CartDetail cartDetail = optionalCartDetail.get();
+            Cart cart = cartDetail.getCart();
+            this.cartDetailRepository.deleteById(cartDetailId);
+
+            if (cart.getSum() > 1) {
+                int newSum = cart.getSum() - 1;
+                cart.setSum(newSum);
+                session.setAttribute("sum", newSum);
+                this.cartRepository.save(cart);
+            } else {
+                this.cartRepository.delete(cart);
+                session.setAttribute("sum", 0);
+            }
+        }
     }
 }
